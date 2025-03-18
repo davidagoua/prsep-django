01d2ea7f35ec827e7553577a49483f9f7eabe6ba
root <daniel.ntaye@sahanalytics.com>
2025-03-18 11:08:33 +0000

    Add programme app with migrations, models, and admin setup
    Initializes the `programme` app with core models, migrations, and admin definitions. Establishes relationships between entities such as activities, actions, and indicators. Multiple migrations implement further refinements and enhancements such as adding fields, altering field behavior, and cleaning redundant models.

--- programme/views.py
+++ programme/views.py
@@ -1,9 +1,19 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.views import generic
 
+from programme.models import ComposantesProgram
 from test import ingest
 
 
 def ingest_data(request, sheet_name):
     response = ingest(sheet_name)
     return JsonResponse({'status': 'ok', 'data': response})
+
+
+class SuiviTemplateView(generic.TemplateView):
+    template_name = 'suivi/ptba-programme.html'
+
+    def get_context_data(self, **kwargs):
+        composants = ComposantesProgram.objects.all()
+        return kwargs | locals()
