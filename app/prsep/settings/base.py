674fe4d365ad5c33474a8f092b93e3420473a2c9
root <daniel.ntaye@sahanalytics.com>
2025-03-23 12:33:40 +0000

    adding collabora

--- app/prsep/settings/base.py
+++ app/prsep/settings/base.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-a%!!z9$&zosed-v4nt(_u^*rl6ups&nqw9j4pl*1u(7056vz&p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.201.28','localhost']
+ALLOWED_HOSTS = ['192.168.201.28','localhost','smartshopper.uno']
 
 
 # Application definition
