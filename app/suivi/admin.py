0184d8fed15bb06bc317d578aebe6326a71359ed
root <daniel.ntaye@sahanalytics.com>
2025-03-24 10:54:35 +0000

    Add programme app with migrations, models, and admin setup
    Initializes the `programme` app with core models, migrations, and admin definitions. Establishes relationships between entities such as activities, actions, and indicators. Multiple migrations implement further refinements and enhancements such as adding fields, altering field behavior, and cleaning redundant models.

--- app/suivi/admin.py
+++ app/suivi/admin.py
@@ -4,10 +4,10 @@ from .models import CommentaireTDR, TDR
 
 @admin.register(TDR)
 class TDRAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'label','activity', 'created', 'state')
+    list_display = ('id', 'user', 'label','activity', 'created', 'state', 'injonction', 'accorder')
     list_filter = ('state',)
 
 
 @admin.register(CommentaireTDR)
 class CommentaireTDRAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'created', 'content')
+    list_display = ('id', 'user', 'created', 'content', 'injonction', 'accorder')
