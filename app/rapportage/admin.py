ff31c54ebeea1776b213e0d2d3c9c1d6c1ea01ec
macbookpro <cdavidagoua@gmail.com>
2025-10-14 00:12:38 +0000

    Add backend service as a submodule

--- app/rapportage/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Rapport
-
-
-@admin.register(Rapport)
-class RapportAdmin(admin.ModelAdmin):
-    list_display = ('label', 'state','type')
-    list_filter = ('state',)
--- app/rapportage/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RapportageConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "rapportage"
